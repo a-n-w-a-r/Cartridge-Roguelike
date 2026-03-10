2d553ef93e3eddab0e27389ba637b716da21da33
anwar <anwarfs@proton.me>
2026-03-10 22:05:37 +0000

    Removed redundant code

--- src/main/java/chrollo/roguelike/App.java
+++ src/main/java/chrollo/roguelike/App.java
@@ -15,8 +15,6 @@ public class App extends Application {
 
     final Pane root = new Pane();
 
-    final Pane deckPane = new Pane();
-
     ArrayList<Character> deck = new ArrayList<>();
 
     private Parent createContent() {
@@ -26,41 +24,16 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        Character[] myDeck = {
-//                new Character(100, 20, "Knight", "knight.png", Color.BEIGE),
-//                new Character(50, 40, "Mage", "mage.png", Color.ORANGE),
-//                new Character(80, 25, "Archer", "archer.png", Color.AQUA)
-//        };
-
-//        deck.add(new Character(100, 20, "Knight", "knight.png", Color.BEIGE));
-//        deck.add(new Character(50, 40, "Mage", "mage.png", Color.ORANGE));
-//        deck.add(new Character(80, 25, "Archer", "archer.png", Color.AQUA));
 
         Deck deckUi = new Deck();
 
-        deckUi.setLayoutX(240); // Centering a 480 width pane in a 960 window
+        deckUi.setLayoutX(240);
         deckUi.setLayoutY(290);
 
-//        showDeck(deck, deckPane);
         root.getChildren().add(deckUi);
         Scene scene = new Scene(createContent());
         stage.setTitle("Roguelike");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public void showDeck(ArrayList<Character> deck, Pane root) {
-
-        double offset = 100;
-
-        for (Character c : deck) {
-            Character.Card showCard = new Character.Card(c);
-            root.getChildren().add(showCard);
-
-            showCard.setX(offset);
-            offset+= 100;
-        }
     }
 }
